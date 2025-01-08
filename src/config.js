ec9488e85e88707c7687dab1cc848e7d29cc48a2
Berkay AY <61064047+shadesofdeath@users.noreply.github.com>
2025-01-08 19:32:46 +0300

    👍

--- src/config.js
+++ src/config.js
@@ -1,217 +1,217 @@
 // Windows version configuration
 exports.UUP_TABLE = [
-    {
-      updateId: 'ad27e52b-9e18-408a-9df2-8688e5273fbf',
-      version: 'W11 24H2',
-      arch: 'x64',
-      isWin11: true
-    },
-    {
-      updateId: 'b6049aaf-4f56-4183-8e58-32954906de64',
-      version: 'W11 24H2',
-      arch: 'arm64',
-      isWin11: true
-    },
-    {
-      updateId: '30da46b4-2ff2-4682-a9ae-23b66dd98713',
-      version: 'W11 Server version 24H2',
-      arch: 'x64',
-      isWin11: true
-    },
-    {
-      updateId: 'fb12fdae-dd42-4d2d-b8cc-380a968e8d10',
-      version: 'W11 Server version 24H2',
-      arch: 'arm64',
-      isWin11: true
-    },
-    {
-      updateId: 'ebf14a71-0f6e-4958-9bf4-27c746050640',
-      version: 'W11 Server version 23H2',
-      arch: 'x64',
-      isWin11: true
-    },
-    {
-      updateId: '9310ebcc-79ee-40aa-ae4a-4da849dd4684',
-      version: 'W10 Server version 22H2',
-      arch: 'x64',
-      isWin11: false
-    },
-    {
-      updateId: '2ba1d737-a36b-415b-a630-85bd5146d77d',
-      version: 'W10 Server version 21H2',
-      arch: 'x64',
-      isWin11: false
-    },
-    {
-      updateId: '0c36c0e6-8397-424d-b460-26cc0d044c7f',
-      version: 'W11 22H2 & 23H2',
-      arch: 'x64',
-      isWin11: true
-    },
-    {
-      updateId: 'a2b2e765-4612-44d8-9fb0-b11b699aaf4b',
-      version: 'W11 22H2 & 23H2',
-      arch: 'arm64',
-      isWin11: true
-    },
-    {
-      updateId: '35630b7b-4509-45b6-83a1-d4c75d5aa9b6',
-      version: 'W11 21H2',
-      arch: 'x64',
-      isWin11: true
-    },
-    {
-      updateId: 'fba7e852-3fcc-4dad-8561-42c455cfffd7',
-      version: 'W11 21H2',
-      arch: 'arm64',
-      isWin11: true
-    },
-    {
-      updateId: 'e1885854-aea0-453d-9e64-bfd00535a925',
-      version: 'W10 22H2',
-      arch: 'x64',
-      isWin11: false
-    },
-    {
-      updateId: '21dec2dc-8213-4021-a31c-7dd07e034dd5',
-      version: 'W10 22H2',
-      arch: 'x86',
-      isWin11: false
-    },
-    {
-      updateId: 'd1aaf6a7-c80b-49bb-8d79-99b6a3cfb5c1',
-      version: 'W10 22H2',
-      arch: 'arm64',
-      isWin11: false
-    },
-    {
-      updateId: '3ead9b43-aa9d-4973-8195-24be0b0ce1e1',
-      version: 'W10 2004, 20H2, 21H1, 21H2',
-      arch: 'x64',
-      isWin11: false
-    },
-    {
-      updateId: '70f4293c-6eaa-4db5-b059-9755ab5628d8',
-      version: 'W10 2004, 20H2, 21H1, 21H2',
-      arch: 'x86',
-      isWin11: false
-    },
-    {
-      updateId: 'bb720c43-af68-4dc6-a397-42f278183314',
-      version: 'W10 2004, 20H2, 21H1, 21H2',
-      arch: 'arm64',
-      isWin11: false
-    },
-    {
-      updateId: '9005d4cd-fce9-466b-b98e-a67414acebd8',
-      version: 'W10 1809',
-      arch: 'x64',
-      isWin11: false
-    },
-    {
-      updateId: 'dcb2f7dd-8822-444b-802f-d417e19474a9',
-      version: 'W10 1809',
-      arch: 'x86',
-      isWin11: false
-    },
-    {
-      updateId: '5a143235-5474-4cfa-9078-65e434d9df67',
-      version: 'W10 1809',
-      arch: 'arm64',
-      isWin11: false
-    }
-  ];
-  
-  // Language configuration
-  exports.REGION_TABLE = [
-    { region: 'ar-sa', language: 'Arabic' },
-    { region: 'bg-bg', language: 'Bulgarian' },
-    { region: 'cs-cz', language: 'Czech' },
-    { region: 'da-dk', language: 'Danish' },
-    { region: 'de-de', language: 'German' },
-    { region: 'el-gr', language: 'Greek' },
-    { region: 'en-gb', language: 'English (United Kingdom)' },
-    { region: 'en-us', language: 'English (United States)' },
-    { region: 'es-es', language: 'Spanish' },
-    { region: 'es-mx', language: 'Spanish (Mexico)' },
-    { region: 'et-ee', language: 'Estonian' },
-    { region: 'fi-fi', language: 'Finnish' },
-    { region: 'fr-ca', language: 'French (Canada)' },
-    { region: 'fr-fr', language: 'French' },
-    { region: 'he-il', language: 'Hebrew' },
-    { region: 'hr-hr', language: 'Croatian' },
-    { region: 'hu-hu', language: 'Hungarian' },
-    { region: 'it-it', language: 'Italian' },
-    { region: 'ja-jp', language: 'Japanese' },
-    { region: 'ko-kr', language: 'Korean' },
-    { region: 'lt-lt', language: 'Lithuanian' },
-    { region: 'lv-lv', language: 'Latvian' },
-    { region: 'nb-no', language: 'Norwegian' },
-    { region: 'nl-nl', language: 'Dutch' },
-    { region: 'pl-pl', language: 'Polish' },
-    { region: 'pt-br', language: 'Portuguese (Brazil)' },
-    { region: 'pt-pt', language: 'Portuguese' },
-    { region: 'ro-ro', language: 'Romanian' },
-    { region: 'ru-ru', language: 'Russian' },
-    { region: 'sk-sk', language: 'Slovak' },
-    { region: 'sl-si', language: 'Slovenian' },
-    { region: 'sr-latn-rs', language: 'Serbian (Latin)' },
-    { region: 'sv-se', language: 'Swedish' },
-    { region: 'th-th', language: 'Thai' },
-    { region: 'tr-tr', language: 'Turkish' },
-    { region: 'uk-ua', language: 'Ukrainian' },
-    { region: 'zh-cn', language: 'Chinese (Simplified)' },
-    { region: 'zh-tw', language: 'Chinese (Traditional)' }
-  ];
-  
-  // Server FOD keywords
-  exports.SERVER_FOD_KEYWORDS = [
-    'Microsoft-Windows-LanguageFeatures-Basic',
-    'Microsoft-Windows-LanguageFeatures-OCR',
-    'Microsoft-Windows-LanguageFeatures-TextToSpeech',
-    'Microsoft-Windows-Client-LanguagePack',
-    'Microsoft-AzureStack-HCI-Management-Tools-FOD-Package',
-    'Microsoft-OneCore-StorageManagement-FoD-Package',
-    'Microsoft-Windows-ActiveDirectory-DS-LDS-Tools-FoD-Package',
-    'Microsoft-Windows-BitLocker-Recovery-Tools-FoD-Package',
-    'Microsoft-Windows-CertificateServices-Tools-FoD-Package',
-    'Microsoft-Windows-Console-Host-Legacy-FoD-Package',
-    'Microsoft-Windows-DHCP-Tools-FoD-Package',
-    'Microsoft-Windows-DNS-Tools-FoD-Package',
-    'Microsoft-Windows-EMS-SAC-Desktop-Tools-FoD-Package',
-    'Microsoft-Windows-FailoverCluster-Management-Tools-FOD-Package',
-    'Microsoft-Windows-FileServices-Tools-FoD-Package',
-    'Microsoft-Windows-GroupPolicy-Management-Tools-FoD-Package',
-    'Microsoft-Windows-IPAM-Client-FoD-Package',
-    'Microsoft-Windows-IRDA-Package',
-    'Microsoft-Windows-NetworkController-Tools-FoD-Package',
-    'Microsoft-Windows-NetworkLoadBalancing-Tools-FoD-Package',
-    'Microsoft-Windows-RasCMAK-Client-Package',
-    'Microsoft-Windows-RasRip-Package',
-    'Microsoft-Windows-RemoteAccess-Management-Tools-FoD-Package',
-    'Microsoft-Windows-RemoteDesktop-Services-Tools-FoD-Package',
-    'Microsoft-Windows-Server-AppCompat-FoD-Package',
-    'Microsoft-Windows-ServerCoreFonts-NonCritical-Fonts-BitmapFonts-FOD-Package',
-    'Microsoft-Windows-ServerCoreFonts-NonCritical-Fonts-MinConsoleFonts-FOD-Package',
-    'Microsoft-Windows-ServerCoreFonts-NonCritical-Fonts-Support-FOD-Package',
-    'Microsoft-Windows-ServerCoreFonts-NonCritical-Fonts-TrueType-FOD-Package',
-    'Microsoft-Windows-ServerCoreFonts-NonCritical-Fonts-UAPFonts-FOD-Package',
-    'Microsoft-Windows-ServerManager-Tools-FoD-Package',
-    'Microsoft-Windows-SNMP-Client-Package',
-    'Microsoft-Windows-StorageManagement-FoD-Package',
-    'Microsoft-Windows-StorageMigrationService-Management-Tools-FOD-Package',
-    'Microsoft-Windows-StorageReplica-Tools-FoD-Package',
-    'Microsoft-Windows-SystemInsights-Management-Tools-FOD-Package',
-    'Microsoft-Windows-TPM-Diagnostics-FOD-Package',
-    'Microsoft-Windows-VolumeActivation-Tools-FoD-Package',
-    'Microsoft-Windows-Shielded-VM-Tools-FoD-Package',
-    'Microsoft-Windows-WMI-SNMP-Provider-Client-Package',
-    'Microsoft-OneCore-DeveloperMode-Desktop-Package',
-    'microsoft-windows-composition-test-fod-package',
-    'microsoft-windows-ipam-client-fod-package',
-    'microsoft-windows-irda-package',
-    'microsoft-windows-systeminsights-management-tools-fod-package',
-    'microsoft-windows-wmi-snmp-provider-client-package',
-    'microsoft-windows-media-features-package',
-    'Microsoft-Windows-WSUS-Tools-FoD-Package'
-  ];
+  {
+    updateId: 'ad27e52b-9e18-408a-9df2-8688e5273fbf',
+    version: 'W11 24H2',
+    arch: 'x64',
+    isWin11: true
+  },
+  {
+    updateId: 'b6049aaf-4f56-4183-8e58-32954906de64',
+    version: 'W11 24H2',
+    arch: 'arm64',
+    isWin11: true
+  },
+  {
+    updateId: '30da46b4-2ff2-4682-a9ae-23b66dd98713',
+    version: 'W11 Server version 24H2',
+    arch: 'x64',
+    isWin11: true
+  },
+  {
+    updateId: 'fb12fdae-dd42-4d2d-b8cc-380a968e8d10',
+    version: 'W11 Server version 24H2',
+    arch: 'arm64',
+    isWin11: true
+  },
+  {
+    updateId: 'ebf14a71-0f6e-4958-9bf4-27c746050640',
+    version: 'W11 Server version 23H2',
+    arch: 'x64',
+    isWin11: true
+  },
+  {
+    updateId: '9310ebcc-79ee-40aa-ae4a-4da849dd4684',
+    version: 'W10 Server version 22H2',
+    arch: 'x64',
+    isWin11: false
+  },
+  {
+    updateId: '2ba1d737-a36b-415b-a630-85bd5146d77d',
+    version: 'W10 Server version 21H2',
+    arch: 'x64',
+    isWin11: false
+  },
+  {
+    updateId: '0c36c0e6-8397-424d-b460-26cc0d044c7f',
+    version: 'W11 22H2 & 23H2',
+    arch: 'x64',
+    isWin11: true
+  },
+  {
+    updateId: 'a2b2e765-4612-44d8-9fb0-b11b699aaf4b',
+    version: 'W11 22H2 & 23H2',
+    arch: 'arm64',
+    isWin11: true
+  },
+  {
+    updateId: '35630b7b-4509-45b6-83a1-d4c75d5aa9b6',
+    version: 'W11 21H2',
+    arch: 'x64',
+    isWin11: true
+  },
+  {
+    updateId: 'fba7e852-3fcc-4dad-8561-42c455cfffd7',
+    version: 'W11 21H2',
+    arch: 'arm64',
+    isWin11: true
+  },
+  {
+    updateId: 'e1885854-aea0-453d-9e64-bfd00535a925',
+    version: 'W10 22H2',
+    arch: 'x64',
+    isWin11: false
+  },
+  {
+    updateId: '21dec2dc-8213-4021-a31c-7dd07e034dd5',
+    version: 'W10 22H2',
+    arch: 'x86',
+    isWin11: false
+  },
+  {
+    updateId: 'd1aaf6a7-c80b-49bb-8d79-99b6a3cfb5c1',
+    version: 'W10 22H2',
+    arch: 'arm64',
+    isWin11: false
+  },
+  {
+    updateId: '3ead9b43-aa9d-4973-8195-24be0b0ce1e1',
+    version: 'W10 2004, 20H2, 21H1, 21H2',
+    arch: 'x64',
+    isWin11: false
+  },
+  {
+    updateId: '70f4293c-6eaa-4db5-b059-9755ab5628d8',
+    version: 'W10 2004, 20H2, 21H1, 21H2',
+    arch: 'x86',
+    isWin11: false
+  },
+  {
+    updateId: 'bb720c43-af68-4dc6-a397-42f278183314',
+    version: 'W10 2004, 20H2, 21H1, 21H2',
+    arch: 'arm64',
+    isWin11: false
+  },
+  {
+    updateId: '9005d4cd-fce9-466b-b98e-a67414acebd8',
+    version: 'W10 1809',
+    arch: 'x64',
+    isWin11: false
+  },
+  {
+    updateId: 'dcb2f7dd-8822-444b-802f-d417e19474a9',
+    version: 'W10 1809',
+    arch: 'x86',
+    isWin11: false
+  },
+  {
+    updateId: '5a143235-5474-4cfa-9078-65e434d9df67',
+    version: 'W10 1809',
+    arch: 'arm64',
+    isWin11: false
+  }
+];
+
+// Language configuration
+exports.REGION_TABLE = [
+  { region: 'ar-sa', language: 'Arabic' },
+  { region: 'bg-bg', language: 'Bulgarian' },
+  { region: 'cs-cz', language: 'Czech' },
+  { region: 'da-dk', language: 'Danish' },
+  { region: 'de-de', language: 'German' },
+  { region: 'el-gr', language: 'Greek' },
+  { region: 'en-gb', language: 'English (United Kingdom)' },
+  { region: 'en-us', language: 'English (United States)' },
+  { region: 'es-es', language: 'Spanish' },
+  { region: 'es-mx', language: 'Spanish (Mexico)' },
+  { region: 'et-ee', language: 'Estonian' },
+  { region: 'fi-fi', language: 'Finnish' },
+  { region: 'fr-ca', language: 'French (Canada)' },
+  { region: 'fr-fr', language: 'French' },
+  { region: 'he-il', language: 'Hebrew' },
+  { region: 'hr-hr', language: 'Croatian' },
+  { region: 'hu-hu', language: 'Hungarian' },
+  { region: 'it-it', language: 'Italian' },
+  { region: 'ja-jp', language: 'Japanese' },
+  { region: 'ko-kr', language: 'Korean' },
+  { region: 'lt-lt', language: 'Lithuanian' },
+  { region: 'lv-lv', language: 'Latvian' },
+  { region: 'nb-no', language: 'Norwegian' },
+  { region: 'nl-nl', language: 'Dutch' },
+  { region: 'pl-pl', language: 'Polish' },
+  { region: 'pt-br', language: 'Portuguese (Brazil)' },
+  { region: 'pt-pt', language: 'Portuguese' },
+  { region: 'ro-ro', language: 'Romanian' },
+  { region: 'ru-ru', language: 'Russian' },
+  { region: 'sk-sk', language: 'Slovak' },
+  { region: 'sl-si', language: 'Slovenian' },
+  { region: 'sr-latn-rs', language: 'Serbian (Latin)' },
+  { region: 'sv-se', language: 'Swedish' },
+  { region: 'th-th', language: 'Thai' },
+  { region: 'tr-tr', language: 'Turkish' },
+  { region: 'uk-ua', language: 'Ukrainian' },
+  { region: 'zh-cn', language: 'Chinese (Simplified)' },
+  { region: 'zh-tw', language: 'Chinese (Traditional)' }
+];
+
+// Server FOD keywords
+exports.SERVER_FOD_KEYWORDS = [
+  'Microsoft-Windows-LanguageFeatures-Basic',
+  'Microsoft-Windows-LanguageFeatures-OCR',
+  'Microsoft-Windows-LanguageFeatures-TextToSpeech',
+  'Microsoft-Windows-Client-LanguagePack',
+  'Microsoft-AzureStack-HCI-Management-Tools-FOD-Package',
+  'Microsoft-OneCore-StorageManagement-FoD-Package',
+  'Microsoft-Windows-ActiveDirectory-DS-LDS-Tools-FoD-Package',
+  'Microsoft-Windows-BitLocker-Recovery-Tools-FoD-Package',
+  'Microsoft-Windows-CertificateServices-Tools-FoD-Package',
+  'Microsoft-Windows-Console-Host-Legacy-FoD-Package',
+  'Microsoft-Windows-DHCP-Tools-FoD-Package',
+  'Microsoft-Windows-DNS-Tools-FoD-Package',
+  'Microsoft-Windows-EMS-SAC-Desktop-Tools-FoD-Package',
+  'Microsoft-Windows-FailoverCluster-Management-Tools-FOD-Package',
+  'Microsoft-Windows-FileServices-Tools-FoD-Package',
+  'Microsoft-Windows-GroupPolicy-Management-Tools-FoD-Package',
+  'Microsoft-Windows-IPAM-Client-FoD-Package',
+  'Microsoft-Windows-IRDA-Package',
+  'Microsoft-Windows-NetworkController-Tools-FoD-Package',
+  'Microsoft-Windows-NetworkLoadBalancing-Tools-FoD-Package',
+  'Microsoft-Windows-RasCMAK-Client-Package',
+  'Microsoft-Windows-RasRip-Package',
+  'Microsoft-Windows-RemoteAccess-Management-Tools-FoD-Package',
+  'Microsoft-Windows-RemoteDesktop-Services-Tools-FoD-Package',
+  'Microsoft-Windows-Server-AppCompat-FoD-Package',
+  'Microsoft-Windows-ServerCoreFonts-NonCritical-Fonts-BitmapFonts-FOD-Package',
+  'Microsoft-Windows-ServerCoreFonts-NonCritical-Fonts-MinConsoleFonts-FOD-Package',
+  'Microsoft-Windows-ServerCoreFonts-NonCritical-Fonts-Support-FOD-Package',
+  'Microsoft-Windows-ServerCoreFonts-NonCritical-Fonts-TrueType-FOD-Package',
+  'Microsoft-Windows-ServerCoreFonts-NonCritical-Fonts-UAPFonts-FOD-Package',
+  'Microsoft-Windows-ServerManager-Tools-FoD-Package',
+  'Microsoft-Windows-SNMP-Client-Package',
+  'Microsoft-Windows-StorageManagement-FoD-Package',
+  'Microsoft-Windows-StorageMigrationService-Management-Tools-FOD-Package',
+  'Microsoft-Windows-StorageReplica-Tools-FoD-Package',
+  'Microsoft-Windows-SystemInsights-Management-Tools-FOD-Package',
+  'Microsoft-Windows-TPM-Diagnostics-FOD-Package',
+  'Microsoft-Windows-VolumeActivation-Tools-FoD-Package',
+  'Microsoft-Windows-Shielded-VM-Tools-FoD-Package',
+  'Microsoft-Windows-WMI-SNMP-Provider-Client-Package',
+  'Microsoft-OneCore-DeveloperMode-Desktop-Package',
+  'microsoft-windows-composition-test-fod-package',
+  'microsoft-windows-ipam-client-fod-package',
+  'microsoft-windows-irda-package',
+  'microsoft-windows-systeminsights-management-tools-fod-package',
+  'microsoft-windows-wmi-snmp-provider-client-package',
+  'microsoft-windows-media-features-package',
+  'Microsoft-Windows-WSUS-Tools-FoD-Package'
+];
